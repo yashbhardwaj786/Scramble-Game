cb62767ae2e80f34bc6769ef7b124c8ca6fd7629
Yash Bhardwaj <yashbhardwaj@Yashs-MBP.Dlink>
2022-06-24 01:47:10 +0530

    html file and js for game

---
+++ game.js
@@ -0,0 +1,58 @@
+const msg = document.querySelector('.msg');
+const guess = document.querySelector('input');
+const btn = document.querySelector('.btn')
+let play = false;
+let newWords = "";
+let randWords = "";
+
+
+let sWords = ['python', 'javascript', 'c++', 'php', 'java', 'c#', 'html', 'css', 'reactjs', 'angular', 'swift', 'android', 'sql', 'kotlin', 'xml', 'jsp', 'servlet', 'rmi'];
+
+
+const createNewWords = () => {
+    let ranNum = Math.floor(Math.random() * sWords.length);
+    // console.log(ranNum)
+    let newTempSwords = sWords[ranNum];
+    // console.log(newTempSwords.split(""));
+    return newTempSwords;
+}
+
+const scrableWords = (arr) => {
+    for(let i = arr.length-1; i >0; i--) {
+        let temp = arr[i];
+        // console.log(temp);
+        let j = Math.floor(Math.random()*(i+1));
+        // console.log(i);
+        // console.log(j);
+
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    return arr;
+}
+
+
+btn.addEventListener('click', function(){
+    if(!play) {
+        play = true;
+        btn.innerHTML = "Guess";
+        guess.classList.toggle('hidden');
+        newWords = createNewWords();
+        randWords = scrableWords(newWords.split("")).join("");
+        // console.log(randWords.join(""));
+        msg.innerHTML = `Guess the Word: ${randWords}`;
+    } else {
+        let tempWord = guess.value;
+        if(tempWord === newWords){
+            // console.log('correct');
+            play = false;
+            msg.innerHTML = `Awesome It's Correct. it is ${newWords}`;
+            btn.innerHTML = "Start Again";
+            guess.classList.toggle('hidden');
+            guess.value = "";
+        }else {
+            console.log('incorrect');
+            msg.innerHTML = `Sorry Boss. It's not Correct. Please try again ${randWords}`;
+        }
+    }
+})
